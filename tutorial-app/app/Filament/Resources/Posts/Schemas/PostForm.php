c40301d5a6b7dfa7159eaf1094b7d1ac6c9f8f1a
ArchchanaKrishnathas <archchana2504@gmail.com>
2026-03-05 09:21:34 +0530

    Refactor Post form into Sections and Groups

--- tutorial-app/app/Filament/Resources/Posts/Schemas/PostForm.php
+++ tutorial-app/app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -11,7 +11,10 @@ use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
 
 class PostForm
 {
@@ -19,18 +22,38 @@ class PostForm
     {
         return $schema
             ->components([
-                TextInput::make("title"),
-                TextInput::make("slug"),
-                // Select::make("category_id")->options(["one","two"])
-                Select::make("category_id")
-                        ->label("Category")
-                        ->options(Category::all()->pluck("name","id")),
-                ColorPicker::make("color"),
-                MarkdownEditor::make("body"),
-                FileUpload::make("image")->disk("public")->directory("posts"),
-                TagsInput::make("tags"),
-                Checkbox::make("published"),
-                DatePicker::make("published_at")
-            ])->columns(2);
+                Section::make("Fields")
+                    ->description("Fill all the fields")
+                    ->icon(Heroicon::RocketLaunch)
+                    ->schema([
+                        Group::make()
+                            ->schema([
+                                TextInput::make("title"),
+                                TextInput::make("slug"),
+                                // Select::make("category_id")->options(["one","two"])
+                                Select::make("category_id")
+                                        ->label("Category")
+                                        ->options(Category::all()->pluck("name","id")),
+                                ColorPicker::make("color")
+                            ])->columns(2),
+                        MarkdownEditor::make("body")
+                     ])->columnSpan(2),
+                    // ])->columnSpanFull(),
+
+                Group::make()
+                    ->schema([
+                        Section::make("Image Upload")
+                            ->schema([
+                                FileUpload::make("image")->disk("public")->directory("posts")
+                            ]),
+                        Section::make("Meta")
+                            ->schema([
+                                TagsInput::make("tags"),
+                                Checkbox::make("published"),
+                                DatePicker::make("published_at")
+                            ])
+                    ])->columnSpan(1)
+
+                ])->columns(3);
     }
 }
